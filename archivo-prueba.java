90561c2f3facd4fa6da3bd703469932855abf644
Luis-Solano97 <72114946+Luis-Solano97@users.noreply.github.com>
2021-01-12 22:02:27 -0600

    conversor

--- archivo-prueba.java
+++ archivo-prueba.java
@@ -1,1 +1,31 @@
-intentar hacer git push
+/*
+ Programa decimal a binario
+ Marin Solano Luis Andrés
+ Rodríguez Cedeño Aldo
+ */
+import java.util.Scanner;
+
+public class Binario{
+
+    public static void main(String[] args) {
+
+        int numero, exp, digito;
+        double binario;
+        Scanner sc = new Scanner(System.in);
+
+        do{ 
+            System.out.print("Introduce un numero entero: ");                                                
+            numero = sc.nextInt();
+        }while(numero < 0);
+
+        exp=0;
+        binario=0;
+        while(numero!=0){
+                digito = numero % 2;           
+                binario = binario + digito * Math.pow(10, exp);                                                   
+                exp++;
+                numero = numero/2;
+        }
+        System.out.printf("Binario: %.0f %n", binario);
+    }
+}
